bfb4f75f4aa323128d636caacc211b23d847b435
Mikoyzskie <rapidstrike13@gmail.com>
2024-03-10 15:32:25 +0800

    update public routes

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,7 +2,7 @@ import { authMiddleware } from "@clerk/nextjs";
 
 export default authMiddleware({
   // Routes that can be accessed while signed out
-  publicRoutes: ["/", "/jobs"],
+  publicRoutes: ["/", "/jobs", "/jobs/:path*"],
   // Routes that can always be accessed, and have
   // no authentication information
 });
